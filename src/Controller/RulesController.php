feee38d5175b8687f0642f9ae615aadddf47e4a3
Hikari Ylan <ylan.nicolas52600@gmail.com>
2024-12-31 02:19:16 +0100

    Adding extensions rules pages

--- src/Controller/RulesController.php
+++ src/Controller/RulesController.php
@@ -15,4 +15,36 @@ class RulesController extends AbstractController
             'controller_name' => 'RulesController',
         ]);
     }
+
+    #[Route('/regles/trash', name: 'app_rules_trash')]
+    public function trash(): Response
+    {
+        return $this->render('rules/trash.html.twig', [
+            'controller_name' => 'RulesController',
+        ]);
+    }
+
+    #[Route('/regles/apocalypse', name: 'app_rules_apocalypse')]
+    public function apocalypse(): Response
+    {
+        return $this->render('rules/apocalypse.html.twig', [
+            'controller_name' => 'RulesController',
+        ]);
+    }
+
+    #[Route('/regles/fantaisie', name: 'app_rules_fantasy')]
+    public function fantaisie(): Response
+    {
+        return $this->render('rules/fantasy.html.twig', [
+            'controller_name' => 'RulesController',
+        ]);
+    }
+
+    #[Route('/regles/girl_power', name: 'app_rules_girl_power')]
+    public function girlPower(): Response
+    {
+        return $this->render('rules/girl_power.html.twig', [
+            'controller_name' => 'RulesController',
+        ]);
+    }
 }
